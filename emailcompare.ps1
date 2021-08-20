d5207ff93dbb1637cc6e70bfd80359a2a2b864c1
abyVa <77400671+abyVa@users.noreply.github.com>
2021-08-20 11:29:40 -0400

    Add files via upload

---
+++ emailcompare.ps1
@@ -0,0 +1,7 @@
+﻿$Email= 'Email11'
+#v1MPL
+
+cls
+$OrdersA = Import-CSv  "C:\Users\VACOBeyenA\Documents\V1DeliverableEmail.csv" -Header "Email11"
+$OrdersB = Import-Csv "C:\Users\VACOBeyenA\Documents\V1MPLP.csv" -Header "Email1"
+Compare-Object $OrdersA $OrdersB -Property $Email | Export-CSV -Path "C:\Users\VACOBeyenA\Documents\outputPath.csv"
