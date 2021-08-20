519c29b0474f4ba097f84a98133abd18e0b281cb
abyVa <77400671+abyVa@users.noreply.github.com>
2021-08-20 11:31:12 -0400

    Add files via upload

---
+++ emailcompare.ps1
@@ -0,0 +1,9 @@
+﻿$Email= 'Email11'
+#v1MPL
+
+#cls
+$strReference  = Import-CSv  "locationtoStore" -Header "Email11"
+$defReference  = Import-Csv "locationStore" -Header "Email11"
+Compare-Object $strReference $defReference -Property $Email | Export-CSV -Path "loactionOutPut"
+
+
